c48c6f5a2a0a0cd6d75b638220be0b01f093f77f
Pavel <wol1414@gmail.com>
2020-07-28 17:02:22 +0300

    remove slider

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -3,59 +3,7 @@
 
 function eventHandler() { 
 	lightGallery(document.getElementById('lightgallery')); 
-	let defaultSlide = {
-		slidesPerView: 2,
-		slidesPerColumn: 3,
-		slidesPerColumnFill: 'row',
-		watchOverflow: true,
-		// slidesPerGroup: 4,
-		spaceBetween: 30,
-		// loop: true,
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-		lazy: {
-			loadPrevNext: true,
-			loadPrevNextAmount: 28,
-		},
-		breakpoints: {
-			// when window width is >= 320px
-			576: {
-				slidesPerView: 3,
-				slidesPerColumn: 3,
-			},
-			// when window width is >= 480px
-			768: {
-				slidesPerView: 4,
-				slidesPerColumn: 3,
-			},
-
-			992: {
-				slidesPerView: 6,
-				slidesPerColumn: 3,
-			},
-
-			// when window width is >= 640px
-			1200: {
-				slidesPerView: 7,
-				slidesPerColumn: 3,
-			},
-
-			1660: {
-				slidesPerView: 8,
-				slidesPerColumn: 3,
-			},
-			1660: {
-				slidesPerView: 9,
-				slidesPerColumn: 3,
-			},
-		}
-	}
-	let swiper = new Swiper('.gal-section__slider--js', {
-		...defaultSlide,
-	
-	});
+  
  
 	// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 	let vh = window.innerHeight * 0.01;
